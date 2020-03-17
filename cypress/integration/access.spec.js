56ff80cac740f5de7eab58274b3d637efc8394f5
tsujimitsu <tsujimitsu@gmail.com>
2020-03-18 00:12:46 +0900

    add cypress

---
+++ cypress/integration/access.spec.js
@@ -0,0 +1,6 @@
+describe('My First Test', function() {
+  it('Visits the Kitchen Sink', function() {
+    cy.visit('http://localhost:8080/a')
+    cy.get('h2')
+  })
+})
--- frontend/src/components/FileUpload.vue
+++ frontend/src/components/FileUpload.vue
@@ -24,6 +24,7 @@ export default {
       e.preventDefault();
       let files = e.target.files;
       this.uploadFile = files[0];
+      console.log(this.uploadFile);
     },
     putPreSignedUrl: async function(fileName, fileType) {
       const { tenantId } = this.$route.params;
@@ -51,11 +52,22 @@ export default {
       // FormData を利用して File を POST する
       //let formData = new FormData()
       //formData.append(this.uploadFile.name, this.uploadFile)
+      const amazDate = new Date()
+        .toISOString()
+        .replace(/[:\\-]|\.\d{3}/g, "")
+        .substr(0, 17);
+
       let config = {
         headers: {
           //'Content-Type': 'multipart/form-data'
           //'Content-Type': fileType
-          "Content-Type": "binary/octet-stream"
+          "Content-Type": "binary/octet-stream",
+          //"X-Amz-Content-Sha256": "UNSIGNED-PAYLOAD",
+          "X-Amz-Content-Sha256":
+            "1359f2a7585ab9e91b4910b35a5b7eefa55ae4cad967b65960d06cb0f4b878dc",
+          "X-Amz-Date": amazDate
+          //"X-Amz-Content-Sha256":
+          //  "1359f2a7585ab9e91b4910b35a5b7eefa55ae4cad967b65960d06cb0f4b878dc"
         }
       };
 
